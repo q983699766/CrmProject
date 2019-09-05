018f88c95358dc877199238dd6703ceb8f21e75d
fanyuxing <1165957853@qq.com>
2019-09-05 15:22:49 +0800

    第一个表的控制器service和serviceimpl

---
+++ CRM/src/main/java/com/sc/service/OfficeChecktargetService.java
@@ -0,0 +1,12 @@
+package com.sc.service;
+
+import java.util.List;
+
+import com.sc.bean.OfficeChecktarget;
+
+public interface OfficeChecktargetService {
+	public List<OfficeChecktarget> selectOfficeChecktargetAll();
+	
+	
+	
+}
