5beaf7c0479e4656317ad4e084d8f6876adb54bb
402812721 <402812721@qq.com>
2019-09-06 17:07:28 +0800

    拉数据

--- CRM/src/main/java/com/sc/service/PurProductsService.java
+++ CRM/src/main/java/com/sc/service/PurProductsService.java
@@ -3,11 +3,11 @@ package com.sc.service;
 import java.util.List;
 
 import com.github.pagehelper.PageInfo;
-import com.sc.bean.CcCkxxb;
 import com.sc.bean.Ccspxxb;
+import com.sc.bean.PurProducts;
 
 public interface PurProductsService {
-	//查询所有的 库存商品信息
+	    //查询所有的 库存商品信息
 		public List<Ccspxxb> selectCcspxx();
 		
 		//库存商品信息分页
@@ -15,6 +15,9 @@ public interface PurProductsService {
 		
 		//和商品联合查询
 		public PageInfo<Ccspxxb> selectproandsp(Integer pageNum, Integer pageSize,Ccspxxb ccspxxb);
+		
+		//查询所有的 库存商品信息
+		public PageInfo<PurProducts> selectpurproducts(Integer pageNum, Integer pageSize);
 
 		
 }
