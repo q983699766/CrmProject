a3cf823e0d556adccfb570228127c8a7605adb2c
lzj-123 <965754744@qq.com>
2019-09-09 20:29:51 +0800

    添加了一个人采购详情页面，及其功能

--- CRM/src/main/java/com/sc/controller/PurProductsInfoController.java
+++ CRM/src/main/java/com/sc/controller/PurProductsInfoController.java
@@ -1,12 +1,15 @@
 package com.sc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sc.service.PurProductsInfoService;
 
+@Controller//注册成bean对象
+@RequestMapping("/pur")
 public class PurProductsInfoController {
 
 	@Autowired
