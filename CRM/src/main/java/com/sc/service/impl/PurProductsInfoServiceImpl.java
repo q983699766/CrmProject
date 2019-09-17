d9f94ded9c49a2cc0d513dfa4729e0f817944b05
lzj-123 <965754744@qq.com>
2019-09-17 21:13:09 +0800

    仓库页面，及增删改查，分类查询，一键删除，模糊查询， 商品页面，及增删改查，一键删除，模糊查询 采购页面，及入库，模糊查询

--- CRM/src/main/java/com/sc/service/impl/PurProductsInfoServiceImpl.java
+++ CRM/src/main/java/com/sc/service/impl/PurProductsInfoServiceImpl.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.sc.bean.Ccspxxb;
+import com.sc.bean.CcspxxbExample;
 import com.sc.bean.PurOrderInfo;
 import com.sc.bean.PurOrderInfoExample;
+import com.sc.bean.CcspxxbExample.Criteria;
 import com.sc.mapper.PurOrderInfoMapper;
 import com.sc.service.PurProductsInfoService;
 @Service
@@ -34,6 +37,24 @@ public class PurProductsInfoServiceImpl implements PurProductsInfoService {
 	@Override
 	public PurOrderInfo selctPurOrderInfoByid(Long purInfoNumber) {
 		return	purOrderInfoMapper.selectByPrimaryKey(purInfoNumber);
+		
+	}
+	//模糊查询
+	@Override
+	public PageInfo<PurOrderInfo> mh(Integer pageNum, Integer pageSize, PurOrderInfo purInfoNumber) {
+		PageHelper.startPage(pageNum,pageSize);		
+		PurOrderInfoExample example = new PurOrderInfoExample();
+		//example.setOrderByClause("PRODUCT_ID DESC");
+		if(purInfoNumber.getWarehouseOrnot()!=null){
+			
+			com.sc.bean.PurOrderInfoExample.Criteria criteria = example.createCriteria();
+			criteria.andWarehouseOrnotLike("%"+purInfoNumber.getWarehouseOrnot()+"%");
+		
+		}
+			
+		List<PurOrderInfo> list = purOrderInfoMapper.selectByExample(example);
+		PageInfo<PurOrderInfo> pi = new PageInfo<PurOrderInfo>(list);
+		return pi;
 		
 	}
 	
