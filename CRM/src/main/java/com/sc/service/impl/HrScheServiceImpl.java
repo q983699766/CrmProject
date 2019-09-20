52133321a1030ab61fff0a3cfa8d7ad063f81c17
chenglongwan <693130047@qq.com>
2019-09-20 19:59:06 +0800

    基本完成了功能，還有bug慢慢優化

--- CRM/src/main/java/com/sc/service/impl/HrScheServiceImpl.java
+++ CRM/src/main/java/com/sc/service/impl/HrScheServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.sc.bean.OfficeDetailSms;
 import com.sc.bean.OfficeSms;
 import com.sc.bean.OfficeSmsExample;
@@ -15,6 +17,7 @@ import com.sc.mapper.OfficeDetailSmsMapper;
 import com.sc.mapper.OfficeSmsMapper;
 import com.sc.mapper.SysCOMPANYMapper;
 import com.sc.mapper.SysUsersMapper;
+import com.sc.service.HrScheDetailService;
 import com.sc.service.HrScheService;
 
 @Service
@@ -32,11 +35,48 @@ public class HrScheServiceImpl implements HrScheService{
 	@Autowired
 	OfficeDetailSmsMapper officeDetailSmsMapper;
 	
+	@Autowired
+	HrScheDetailService hrScheDetailService;
+	
 	//查询所有
 	@Override
-	public List<OfficeSms> selectByExample(OfficeSmsExample example) {
+	public PageInfo<OfficeDetailSms> selectByExample(Integer pageNum, Integer pageSize,OfficeSmsExample example,Long userId,String smsHeadline,OfficeSms officeSms) {
+		System.out.println("得到的example為：：：===="+example);
+		System.out.println("得到的officeSms為：：：===="+officeSms);
+		PageHelper.startPage(pageNum, pageSize);
+		List<OfficeDetailSms> selectByExample;
+		if(smsHeadline!=null&&smsHeadline!=""){
+			/*officeSms.setSmsHeadline(smsHeadline);*/
+			officeSms.setSmsSender(userId);
+			System.out.println("进入模糊查询set后得到的officeSms為：：：===="+officeSms);
+			selectByExample = officeDetailSmsMapper.selectdetailsmsAndmysms(officeSms);
+		}else{
+			 selectByExample = officeDetailSmsMapper.selectdetailsmsAnduser(userId);
+		}
 		
-		return this.officeSmsMapper.selectByExample(null);
+			
+		//selectByExample = officeDetailSmsMapper.selectByExample(null);
+		
+		
+		PageInfo<OfficeDetailSms> pageInfo = new PageInfo<OfficeDetailSms>(selectByExample);
+		
+		List<com.sc.bean.OfficeDetailSms> list = pageInfo.getList();
+		for (OfficeDetailSms officeDetailSms2 : list) {
+			Long smsId = officeDetailSms2.getSmsId();
+			Long receiverId = officeDetailSms2.getReceiverId();
+			Long comId = officeDetailSms2.getComId();
+			
+			SysCOMPANY seleCOMById = hrScheDetailService.seleCOMById(comId);
+			OfficeSms selesmsById = hrScheDetailService.selesmsById(smsId);
+			SysUsers seleuserById = hrScheDetailService.seleuserById(receiverId);
+			
+			System.out.println(seleCOMById);
+			officeDetailSms2.setSyscompany(seleCOMById);
+			officeDetailSms2.setSysUsers(seleuserById);
+			officeDetailSms2.setOfficeSms(selesmsById);	
+		}
+		pageInfo.setList(list);
+		return pageInfo;
 	}
 
 	@Override
@@ -80,5 +120,54 @@ public class HrScheServiceImpl implements HrScheService{
 	public List<SysUsers> selectuserByExample(SysUsersExample example) {
 		
 		return this.sysUsersMapper.selectByExample(null);
+	}
+
+	@Override
+	public OfficeDetailSms selectdetailsmsById(Long detailId) {
+		
+		return officeDetailSmsMapper.selectByPrimaryKey(detailId);
+	}
+
+	@Override
+	public PageInfo<OfficeDetailSms> selectreaded(Integer pageNum, Integer pageSize, OfficeSmsExample example,
+			Long userId, OfficeSms officeSms,String read) {
+		
+		System.out.println("得到的example為：：：===="+example);
+		System.out.println("得到的officeSms為：：：===="+officeSms);
+		PageHelper.startPage(pageNum, pageSize);
+		List<OfficeDetailSms> selectByExample;
+		if(read!=null&&read!=""&&read.equals("1")){
+			System.out.println("进入impl并且read=1");
+			/*officeSms.setSmsHeadline(smsHeadline);*/
+			officeSms.setSmsSender(userId);
+			System.out.println("进入模糊查询set后得到的officeSms為：：：===="+officeSms);
+			selectByExample = officeDetailSmsMapper.selectdetailread(officeSms);
+		}else{
+			System.out.println("进入impl并且read=0");
+				officeSms.setSmsSender(userId);
+			 selectByExample = officeDetailSmsMapper.selectdetailread1(officeSms);
+		}
+		//selectByExample = officeDetailSmsMapper.selectByExample(null);
+		
+		
+		PageInfo<OfficeDetailSms> pageInfo = new PageInfo<OfficeDetailSms>(selectByExample);
+		
+		List<com.sc.bean.OfficeDetailSms> list = pageInfo.getList();
+		for (OfficeDetailSms officeDetailSms2 : list) {
+			Long smsId = officeDetailSms2.getSmsId();
+			Long receiverId = officeDetailSms2.getReceiverId();
+			Long comId = officeDetailSms2.getComId();
+			
+			SysCOMPANY seleCOMById = hrScheDetailService.seleCOMById(comId);
+			OfficeSms selesmsById = hrScheDetailService.selesmsById(smsId);
+			SysUsers seleuserById = hrScheDetailService.seleuserById(receiverId);
+			
+			System.out.println(seleCOMById);
+			officeDetailSms2.setSyscompany(seleCOMById);
+			officeDetailSms2.setSysUsers(seleuserById);
+			officeDetailSms2.setOfficeSms(selesmsById);	
+		}
+		pageInfo.setList(list);
+		return pageInfo;
 	}
 }
